34811d1d0444cbbab3cbc755d1a5482436e103f1
reginPekin <reginbegin@gmail.com>
2019-06-04 23:55:35 +0300

    main index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { App } from "./Components/App";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const methods = {
+  id: 1,
+  answer: "123",
+  codeExample: "console.log(21)"
+};
+ReactDOM.render(<App methods={methods} />, document.getElementById("root"));
