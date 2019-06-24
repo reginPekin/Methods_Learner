be3c8db7b40cc6bcac1b7c3be6dc615de29eeb43
reginPekin <reginbegin@gmail.com>
2019-06-24 21:43:40 +0300

    App - TaskMain & methodsList

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 
-import { Task } from "../Task";
+import { TaskMain } from "../TaskMain";
+import { MethodsList } from "../MethodsList/MethodsList";
+
+import styles from "./App.module.css";
 
 export const App = ({ methods }) => {
   return (
-    <div className="App">
-      <Task methods={methods} />
+    <div className={styles.app}>
+      <MethodsList className={styles.methodsList} methods={methods} />
+      <TaskMain methods={methods} />
     </div>
   );
 };
