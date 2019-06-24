9206047f0c41ba7eb2cf3bde8585182888d7b325
reginPekin <reginbegin@gmail.com>
2019-06-24 21:45:48 +0300

    TaskCondition

--- src/Components/TaskCondition/TaskCondition.js
+++ src/Components/TaskCondition/TaskCondition.js
@@ -1,12 +1,21 @@
 import React from "react";
 
+import { ResultImage } from "../ResultImage";
+
 import styles from "./TaskCondition.module.css";
 
-export const TaskCondition = ({ taskCode }) => {
+export const TaskCondition = ({ task, winnerStyle, loserStyle }) => {
   return (
     <div className={styles.task}>
-      <span>Write result of the: </span>
-      <div className={styles.taskCode}>"{taskCode}"</div>
+      <span className={styles.span}>Write result of the: </span>
+      <div className={styles.taskCode}>
+        <div className={styles.textCode}>{task.codeExample}</div>
+        <ResultImage
+          className={styles.codeResult}
+          winnerStyle={winnerStyle}
+          loserStyle={loserStyle}
+        />
+      </div>
     </div>
   );
 };
