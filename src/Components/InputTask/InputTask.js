e264f152e27cd68ba109f4296fcfe1b74d7875d2
reginPekin <reginbegin@gmail.com>
2019-06-24 21:44:18 +0300

    Input

--- src/Components/InputTask/InputTask.js
+++ src/Components/InputTask/InputTask.js
@@ -1,26 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
+//import cx from "classnames";
+// <div className={cx(style.div, style.div2, {[styles.div2]: isCorrect, [styles.div3]: isFalse })}>
 
 import styles from "./InputTask.module.css";
 
-export const InputTask = ({ answer, text }) => {
+export const InputTask = ({ onSubmit, value, onChange, onFocus }) => {
+  const ref = useRef(null);
+
   return (
     <div className={styles.inputTask}>
-      <input
-        className={styles.input}
-        type="text"
-        placeholder="Write your answer"
-        value={text.value}
-      />
-
-      <button
-        className={styles.inputButton}
-        onClick={() =>
-          text === answer ? alert("Correct!") : alert("Nope, sorry")
-        }
-        onChange={text.onChange}
+      <form
+        className={styles.form}
+        onSubmit={event => {
+          onSubmit(event);
+          ref.current.blur();
+        }}
       >
-        Push
-      </button>
+        <input
+          className={styles.input}
+          type="text"
+          placeholder="Write your answer"
+          ref={ref}
+          value={value}
+          onChange={event => {
+            onChange(event);
+          }}
+          onFocus={onFocus}
+        />
+      </form>
     </div>
   );
 };
