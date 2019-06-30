18350b197c48358634760b6d4a42512f77a9ee21
reginPekin <reginbegin@gmail.com>
2019-06-30 04:23:41 +0300

    seatchInput + changed button color

--- src/Components/MethodsList/MethodsList.js
+++ src/Components/MethodsList/MethodsList.js
@@ -8,47 +8,77 @@ import { changeLoserDisplay } from "../../Utils/changeLoserDisplay";
 import { changeMethod } from "../../Utils/changeMethod";
 import { changeTask } from "../../Utils/changeTask";
 
-export const MethodsListContainer = ({ methods, dispatch }) => {
-  const [openedId, setIsOpened] = useState(null);
+export const MethodsListContainer = ({
+  methods,
+  dispatch,
+  methodId,
+  taskId
+}) => {
+  const [openedId, setIsOpened] = useState(0);
+  const [text, setText] = useState("");
+
   return (
     <section className={styles.methodsList}>
-      <div className={styles.title}>Methods List</div>
-      {methods.map((method, key) => (
-        <div className={styles.flexForTasks} key={key}>
-          <button
-            className={styles.methodSelection}
-            method={method}
-            onClick={() => {
-              setIsOpened(method.id);
-              dispatch(changeWinnerDisplay("none"));
-              dispatch(changeLoserDisplay("none"));
-              dispatch(changeMethod(method.id));
-              dispatch(changeTask(0));
-            }}
-          >
-            {method.name}
-          </button>
-          {openedId === method.id &&
-            method.tasks.map((task, taskKey) => (
+      {/* <div className={styles.title}>Methods List</div> */}
+      <input
+        className={styles.searchInput}
+        type="text"
+        placeholder="Find method"
+        value={text}
+        onChange={event => setText(event.target.value)}
+      />
+      {console.log(text)}
+      {methods.map(
+        (method, key) =>
+          (text === "" || text === method.name.slice(0, text.length)) && (
+            <div className={styles.flexForTasks} key={key}>
               <button
-                className={styles.taskSelection}
-                key={taskKey}
+                style={
+                  method.id === methodId
+                    ? { backgroundColor: "white" }
+                    : { backgroundColor: "var(--red)", color: "white" }
+                }
+                className={styles.methodSelection}
+                method={method}
                 onClick={() => {
-                  dispatch(changeTask(task.id));
+                  setIsOpened(method.id);
                   dispatch(changeWinnerDisplay("none"));
                   dispatch(changeLoserDisplay("none"));
+                  dispatch(changeMethod(method.id));
+                  dispatch(changeTask(0));
                 }}
               >
-                {task.taskName}
+                {method.name}
               </button>
-            ))}
-        </div>
-      ))}
+
+              {openedId === method.id &&
+                method.tasks.map((task, taskKey) => (
+                  <button
+                    style={
+                      task.id === taskId
+                        ? { backgroundColor: "white" }
+                        : { backgroundColor: "var(--red)", color: "white" }
+                    }
+                    className={styles.taskSelection}
+                    key={taskKey}
+                    onClick={() => {
+                      dispatch(changeTask(task.id));
+                      dispatch(changeWinnerDisplay("none"));
+                      dispatch(changeLoserDisplay("none"));
+                    }}
+                  >
+                    {task.taskName}
+                  </button>
+                ))}
+            </div>
+          )
+      )}
     </section>
   );
 };
 
 const mapStateToProps = state => ({
+  methodId: state.methodId,
   taskId: state.taskId
 });
 export const MethodsList = connect(mapStateToProps)(MethodsListContainer);
