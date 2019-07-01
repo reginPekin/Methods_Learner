ecdbb401480b895558c33fd6a6ea8bf2f35de15d
reginPekin <reginbegin@gmail.com>
2019-07-01 03:45:53 +0300

    finger images

--- src/Components/MethodsList/MethodsList.js
+++ src/Components/MethodsList/MethodsList.js
@@ -3,6 +3,9 @@ import { connect } from "react-redux";
 
 import styles from "./MethodsList.module.css";
 
+import FingerUp from "../../images/fingerUp.png";
+import FingerDown from "../../images/fingerDown.png";
+
 import { changeWinnerDisplay } from "../../Utils/changeWinnerDidplay";
 import { changeLoserDisplay } from "../../Utils/changeLoserDisplay";
 import { changeMethod } from "../../Utils/changeMethod";
@@ -27,52 +30,72 @@ export const MethodsListContainer = ({
         value={text}
         onChange={event => setText(event.target.value)}
       />
-      {console.log(text)}
-      {methods.map(
-        (method, key) =>
-          (text === "" || text === method.name.slice(0, text.length)) && (
-            <div className={styles.flexForTasks} key={key}>
-              <button
-                style={
-                  method.id === methodId
-                    ? { backgroundColor: "white" }
-                    : { backgroundColor: "var(--red)", color: "white" }
-                }
-                className={styles.methodSelection}
-                method={method}
-                onClick={() => {
-                  setIsOpened(method.id);
-                  dispatch(changeWinnerDisplay("none"));
-                  dispatch(changeLoserDisplay("none"));
-                  dispatch(changeMethod(method.id));
-                  dispatch(changeTask(0));
-                }}
-              >
-                {method.name}
-              </button>
+      <div className={styles.scroll}>
+        {methods.map(
+          (method, key) =>
+            (text === "" ||
+              text.toLowerCase() === method.name.slice(0, text.length)) && (
+              <div className={styles.flexForTasks} key={key}>
+                <button
+                  style={
+                    method.id === methodId
+                      ? { backgroundColor: "white" }
+                      : { backgroundColor: "var(--red)", color: "white" }
+                  }
+                  className={styles.methodSelection}
+                  method={method}
+                  onClick={() => {
+                    setIsOpened(method.id);
+                    dispatch(changeWinnerDisplay("none"));
+                    dispatch(changeLoserDisplay("none"));
+                    dispatch(changeMethod(method.id));
+                    dispatch(changeTask(0));
+                  }}
+                >
+                  {method.name}
+                </button>
 
-              {openedId === method.id &&
-                method.tasks.map((task, taskKey) => (
-                  <button
-                    style={
-                      task.id === taskId
-                        ? { backgroundColor: "white" }
-                        : { backgroundColor: "var(--red)", color: "white" }
-                    }
-                    className={styles.taskSelection}
-                    key={taskKey}
-                    onClick={() => {
-                      dispatch(changeTask(task.id));
-                      dispatch(changeWinnerDisplay("none"));
-                      dispatch(changeLoserDisplay("none"));
-                    }}
-                  >
-                    {task.taskName}
-                  </button>
-                ))}
-            </div>
-          )
-      )}
+                {openedId === method.id &&
+                  method.tasks.map((task, taskKey) => (
+                    <section className={styles.buttonAndFinger}>
+                      <button
+                        style={
+                          task.id === taskId
+                            ? { backgroundColor: "white" }
+                            : { backgroundColor: "var(--red)", color: "white" }
+                        }
+                        className={styles.taskSelection}
+                        key={taskKey}
+                        onClick={() => {
+                          dispatch(changeTask(task.id));
+                          dispatch(changeWinnerDisplay("none"));
+                          dispatch(changeLoserDisplay("none"));
+                        }}
+                      >
+                        {task.taskName}
+                      </button>
+                      {task.finger && (
+                        <img
+                          src={FingerUp}
+                          key={task.id + taskKey + 10}
+                          width="30px"
+                          alt="FingerUp"
+                        />
+                      )}
+                      {task.finger === false && (
+                        <img
+                          src={FingerDown}
+                          key={task.id + taskKey + 10}
+                          width="30px"
+                          alt="FingerDown"
+                        />
+                      )}
+                    </section>
+                  ))}
+              </div>
+            )
+        )}
+      </div>
     </section>
   );
 };
