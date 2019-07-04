341a560fd86a2209442746444f3e0a8fa03d63ed
reginPekin <reginbegin@gmail.com>
2019-07-04 23:12:23 +0300

    index minus

--- src/Components/methods.js
+++ src/Components/methods.js
@@ -8,7 +8,6 @@ export const methodsObject = [
       {
         taskName: "Task №1",
         id: 0,
-        index: 0,
         codeExample: "nope",
         answer: "nope",
         mark: null
@@ -16,7 +15,6 @@ export const methodsObject = [
       {
         taskName: "Task №2",
         id: 1,
-        index: 1,
         codeExample: "nope2",
         answer: "nope2",
         mark: null
@@ -31,7 +29,6 @@ export const methodsObject = [
       {
         taskName: "Task №1",
         id: 0,
-        index: 2,
         codeExample: "nope",
         answer: "nope",
         mark: null
@@ -39,7 +36,6 @@ export const methodsObject = [
       {
         taskName: "Task №2",
         id: 1,
-        index: 3,
         codeExample: "nope",
         answer: "nope",
         mark: null
@@ -55,7 +51,6 @@ export const methodsObject = [
       {
         taskName: "Task №1",
         id: 0,
-        index: 4,
         codeExample: "Array.of(13);",
         answer: "[13]",
         mark: null
@@ -63,7 +58,6 @@ export const methodsObject = [
       {
         taskName: "Task №2",
         id: 1,
-        index: 5,
         codeExample: "Array.of(1, 3, 5, 'milk');",
         answer: "[13,5]",
         mark: null
@@ -71,7 +65,6 @@ export const methodsObject = [
       {
         taskName: "Task №3",
         id: 2,
-        index: 6,
         codeExample: "Array.of(undefined);",
         answer: "[undefined]",
         mark: null
@@ -88,7 +81,6 @@ export const methodsObject = [
       {
         taskName: "Task №1",
         id: 0,
-        index: 7,
         codeExample: `dsfsdfsdfsddsfsdf`,
         answer: "nope",
         mark: null
@@ -96,7 +88,6 @@ export const methodsObject = [
       {
         taskName: "Task №2",
         id: 1,
-        index: 8,
         codeExample: "nope",
         answer: "nope",
         mark: null
@@ -111,7 +102,6 @@ export const methodsObject = [
       {
         taskName: "Task №1",
         id: 0,
-        index: 9,
         codeExample: `[1, 2, 3, 4, 5].copyWithin(0, 3);`,
         answer: "[4, 5, 3, 4, 5]",
         mark: null
@@ -119,7 +109,6 @@ export const methodsObject = [
       {
         taskName: "Task №2",
         id: 1,
-        index: 10,
         codeExample: "[1, 2, 3, 4, 5].copyWithin(-2, -3, -1);",
         answer: "[1, 2, 3, 3, 4]",
         mark: null
@@ -129,12 +118,12 @@ export const methodsObject = [
   {
     name: "entries()",
     id: 5,
-    hint: `The method returns a new **Array Iterator** object that contains the key/value pairs for each index in the array.`,
+    hint: `The method returns a new **Array Iterator** object that contains the key/value pairs for 
+  he array.`,
     tasks: [
       {
         taskName: "Task №1",
         id: 0,
-        index: 11,
         codeExample: `let array1 = ['a', 'b', 'c']; \r\n
 
         let iterator1 = array1.entries();
@@ -146,7 +135,6 @@ export const methodsObject = [
       {
         taskName: "Task №2",
         id: 1,
-        index: 12,
         codeExample: `var array1 = ['a', 'b', 'c'];
 
         var iterator1 = array1.entries();
@@ -167,7 +155,6 @@ export const methodsObject = [
       {
         taskName: "Task №1",
         id: 0,
-        index: 13,
         codeExample: `[12, 5, 8, 130, 44].every(x => x >= 12);`,
         answer: `false`,
         mark: null
@@ -175,7 +162,6 @@ export const methodsObject = [
       {
         taskName: "Task №2",
         id: 1,
-        index: 14,
         codeExample: `[144, 36, 1, 0, 100].every(x => Number.isInteger(Math.sqrt(x)));`,
         answer: `true`,
         mark: null
@@ -185,12 +171,13 @@ export const methodsObject = [
   {
     name: "fill()",
     id: 7,
-    hint: `The fill() method fills (modifies) all the elements of an array from a start index (default zero) to an end index (default array length) with a static value. It returns the modified array.`,
+    hint: `The fill() method fills (modifies) all the elements of an array from a s
+  ault zero) to an
+  ault array length) with a static value. It returns the modified array.`,
     tasks: [
       {
         taskName: "Task №1",
         id: 0,
-        index: 15,
         codeExample: `var array1 = [1, 2, 3, 4];
         console.log(array1.fill(5, 1));`,
         answer: `[1,5,5,5]`,
@@ -199,7 +186,6 @@ export const methodsObject = [
       {
         taskName: "Task №2",
         id: 1,
-        index: 16,
         codeExample: `var array1 = [2, 9, 0, 4, 6];
         console.log(array1.fill(6));`,
         answer: `[6,6,6,6,6]`,
@@ -208,7 +194,6 @@ export const methodsObject = [
       {
         taskName: "Task №3",
         id: 2,
-        index: 17,
         codeExample: `var array1 = [0, 8, -2, 1.1, 7];
         console.log(array1.fill(0, 6, 9));`,
         answer: `[0,8,-2,1.1,7]`,
@@ -224,7 +209,6 @@ export const methodsObject = [
       {
         taskName: "Task №1",
         id: 0,
-        index: 18,
         codeExample: `[12, 5, 8, 130, 44].every(x => x >= 12);`,
         answer: `false`,
         mark: null
@@ -232,7 +216,6 @@ export const methodsObject = [
       {
         taskName: "Task №2",
         id: 1,
-        index: 19,
         codeExample: `[144, 36, 1, 0, 100].every(x => Number.isInteger(Math.sqrt(x)));`,
         answer: `true`,
         mark: null
