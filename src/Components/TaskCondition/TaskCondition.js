66e8d02c46f86d51ff9cc5d13672b67523c5422f
reginPekin <reginbegin@gmail.com>
2019-07-06 22:18:05 +0300

    delete result image

--- src/Components/TaskCondition/TaskCondition.js
+++ src/Components/TaskCondition/TaskCondition.js
@@ -1,22 +1,15 @@
 import React from "react";
 
-import { ResultImage } from "../ResultImage";
-
 import styles from "./TaskCondition.module.css";
+// import { ResultImage } from "../ResultImage";
 
 export const TaskCondition = ({ task, winnerStyle, loserStyle }) => {
   return (
     <div className={styles.task}>
       <span className={styles.span}>Write result of the: </span>
       <div className={styles.taskCode}>
-        <div>
-          <pre className={styles.textCode}>{task.codeExample}</pre>
-        </div>
-        <ResultImage
-          className={styles.codeResult}
-          winnerStyle={winnerStyle}
-          loserStyle={loserStyle}
-        />
+        <p className={styles.textCode}>{task.codeExample}</p>
+        {/* <div className={styles.imageResult} /> */}
       </div>
     </div>
   );
