d5432f66e228209ac3829947bb2d77e4d8232381
reginPekin <reginbegin@gmail.com>
2019-07-10 07:09:11 +0300

    arrayMethodType, new tasks

--- src/Components/methods.js
+++ src/Components/methods.js
@@ -2,21 +2,23 @@ export const methodsObject = [
   {
     name: "from()",
     id: 0,
+    arrayMethodType: "Array.",
     hint:
       "The method creates a new, shallow-copied **Array** instance from an array-like or iterable object",
     tasks: [
       {
         taskName: "Task №1",
         id: 0,
-        codeExample: "nope",
-        answer: "nope",
+        codeExample: "console.log(Array.from([1, 2, 3], x => x * x));",
+        answer: "[1,4,9]",
         mark: null
       },
       {
         taskName: "Task №2",
         id: 1,
-        codeExample: "nope2",
-        answer: "nope2",
+        codeExample: `const set = new Set(['foo', 'bar', 'baz', 'foo']);
+Array.from(set);`,
+        answer: `["foo","bar","baz"]`,
         mark: null
       }
     ]
@@ -24,20 +26,35 @@ export const methodsObject = [
   {
     name: "isArray()",
     id: 1,
+    arrayMethodType: "Array.",
     hint: "The method determines whether the passed value is an **Array**",
     tasks: [
       {
         taskName: "Task №1",
         id: 0,
-        codeExample: "nope",
-        answer: "nope",
+        codeExample: "Array.isArray([10, 'duck', 3, 9]);",
+        answer: "true",
         mark: null
       },
       {
         taskName: "Task №2",
         id: 1,
-        codeExample: "nope",
-        answer: "nope",
+        codeExample: "Array.isArray(new Array());",
+        answer: "true",
+        mark: null
+      },
+      {
+        taskName: "Task №3",
+        id: 2,
+        codeExample: "Array.isArray(null);",
+        answer: "false",
+        mark: null
+      },
+      {
+        taskName: "Task №4",
+        id: 3,
+        codeExample: "Array.isArray('Array');",
+        answer: "false",
         mark: null
       }
     ]
@@ -45,6 +62,7 @@ export const methodsObject = [
   {
     id: 2,
     name: "of()",
+    arrayMethodType: "Array.",
     hint:
       "The method creates a new **Array** instance from a variable number of arguments, regardless of number or type of the arguments. The difference between **Array.of()** and the **Array** constructor is in the handling of integer arguments: Array.of(7) creates an array with a single element, 7, whereas Array(7) creates an empty array with a length property of 7",
     tasks: [
@@ -74,6 +92,7 @@ export const methodsObject = [
   {
     name: "concat()",
     id: 3,
+    arrayMethodType: "Array.prototype.",
     hint: `The method is used to merge two or more arrays. 
   This method does not change the existing arrays, 
   but instead returns a new array.`,
@@ -122,6 +141,7 @@ console.log(numbers); `,
   {
     name: "copyWithin()",
     id: 4,
+    arrayMethodType: "Array.prototype.",
     hint: `The method shallow copies part of an array to another location in the same array and returns it without modifying its length.`,
     tasks: [
       {
@@ -143,6 +163,7 @@ console.log(numbers); `,
   {
     name: "entries()",
     id: 5,
+    arrayMethodType: "Array.prototype.",
     hint: `The method returns a new **Array Iterator** object that contains the key/value pairs for 
   he array.`,
     tasks: [
@@ -157,20 +178,10 @@ console.log(numbers); `,
       {
         taskName: "Task №2",
         id: 1,
-        codeExample: `var array1 = ['a', 'b', 'c'];
-
-        var iterator1 = array1.entries();
-        
-        iterator1.next().done;
-        
-        console.log(iterator1.next().value);
-        var array1 = ['a', 'b', 'c'];
-
-        var iterator1 = array1.entries();
-        
-        iterator1.next().done;
-        
-        console.log(iterator1.next().value);`,
+        codeExample: `let array1 = ['a', 'b', 'c'];
+let iterator1 = array1.entries();
+iterator1.next().done;
+console.log(iterator1.next().value);`,
         answer: `[1,"b"]`,
         mark: null
       }
@@ -179,6 +190,7 @@ console.log(numbers); `,
   {
     name: "every()",
     id: 6,
+    arrayMethodType: "Array.prototype.",
     hint: `The every() method tests whether all elements in the array pass the test implemented by the provided function. It returns a Boolean value.`,
     tasks: [
       {
@@ -200,6 +212,7 @@ console.log(numbers); `,
   {
     name: "fill()",
     id: 7,
+    arrayMethodType: "Array.prototype.",
     hint: `The fill() method fills (modifies) all the elements of an array from a s
   ault zero) to an
   ault array length) with a static value. It returns the modified array.`,
@@ -208,7 +221,7 @@ console.log(numbers); `,
         taskName: "Task №1",
         id: 0,
         codeExample: `var array1 = [1, 2, 3, 4];
-        console.log(array1.fill(5, 1));`,
+console.log(array1.fill(5, 1));`,
         answer: `[1,5,5,5]`,
         mark: null
       },
@@ -216,7 +229,7 @@ console.log(numbers); `,
         taskName: "Task №2",
         id: 1,
         codeExample: `var array1 = [2, 9, 0, 4, 6];
-        console.log(array1.fill(6));`,
+console.log(array1.fill(6));`,
         answer: `[6,6,6,6,6]`,
         mark: null
       },
@@ -224,7 +237,7 @@ console.log(numbers); `,
         taskName: "Task №3",
         id: 2,
         codeExample: `var array1 = [0, 8, -2, 1.1, 7];
-        console.log(array1.fill(0, 6, 9));`,
+console.log(array1.fill(0, 6, 9));`,
         answer: `[0,8,-2,1.1,7]`,
         mark: null
       }
@@ -233,20 +246,27 @@ console.log(numbers); `,
   {
     name: "filter()",
     id: 8,
+    arrayMethodType: "Array.prototype.",
     hint: `The method creates a new array with all elements that pass the test implemented by the provided function.`,
     tasks: [
       {
         taskName: "Task №1",
         id: 0,
-        codeExample: `[12, 5, 8, 130, 44].every(x => x >= 12);`,
-        answer: `false`,
+        codeExample: `const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+const result = words.filter(word => word.length > 5);
+console.log(result);`,
+        answer: `["exuberant","destruction","present"]`,
         mark: null
       },
       {
         taskName: "Task №2",
         id: 1,
-        codeExample: `[144, 36, 1, 0, 100].every(x => Number.isInteger(Math.sqrt(x)));`,
-        answer: `true`,
+        codeExample: `const fruits = ['apple', 'banana', 'grapes', 'mango', 'orange'];
+const filterItems = (arr, query) => {
+    return arr.filter(el => el.indexOf(query) !== -1);
+};
+console.log(filterItems(fruits, 'ap'));`,
+        answer: `["apple","grapes"]`,
         mark: null
       }
     ]
